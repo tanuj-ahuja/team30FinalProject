1d69b4ce73b964b60597fd6eaa88f7dca1e9bdbb
Tanuj Ahuja <tanujahuja1999@gmail.com>
2022-07-16 19:20:51 -0400

    User signup and login (#1)
    * user signup code
    
    * user signin
    
    Co-authored-by: Tanuj Ahuja <tanuj.ahuja@postman.com>

--- app/src/main/java/com/example/team30finalproject/SignUpActivity.java
+++ app/src/main/java/com/example/team30finalproject/SignUpActivity.java
@@ -3,12 +3,12 @@ package com.example.team30finalproject;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,31 +17,23 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
     private EditText editTextEmail, editTextPassword;
-    private Button signIn;
+    private Button signUp;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
 
         editTextEmail = (EditText) findViewById(R.id.email);
         editTextPassword = (EditText) findViewById(R.id.password);
-        signIn = (Button) findViewById(R.id.login);
-
+        signUp = (Button) findViewById(R.id.sign_up);
         mAuth = FirebaseAuth.getInstance();
 
-        findViewById(R.id.redirect_to_signup).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
-            }
-        });
-
-        signIn.setOnClickListener(new View.OnClickListener() {
+        signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String email = editTextEmail.getText().toString().trim();
@@ -67,15 +59,17 @@ public class MainActivity extends AppCompatActivity {
                     editTextPassword.requestFocus();
                 }
 
-                mAuth.signInWithEmailAndPassword(editTextEmail.getText().toString().trim(), editTextPassword.getText().toString().trim()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                mAuth.createUserWithEmailAndPassword(editTextEmail.getText().toString().trim(), editTextPassword.getText().toString().trim()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
-                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                            startActivity(intent);
+                            Toast.makeText(getApplicationContext(), "User Created", Toast.LENGTH_LONG).show();
                         } else {
-                            Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                            if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                                Toast.makeText(getApplicationContext(), "User Already Registered", Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                            }
                         }
                     }
                 });
@@ -83,4 +77,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+
 }
